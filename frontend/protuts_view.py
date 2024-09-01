edae7328fe421489b54ae9d4834c0bcf7a266380
gaby0805 <gabbiebrabo@gmail.com>
2024-09-01 12:15:14 -0300

    teste

--- frontend/protuts_view.py
+++ frontend/protuts_view.py
@@ -10,22 +10,18 @@ def produtos_view(page: ft.Page):
         pass
 
     
-
     nome_product = ft.TextField(label='nome do produto')
-    # category_product =ft.Dropdown(
-    #     ft.dropdown.Option('1'),
-    #     ft.dropdown.Option('2'),
-    #     ft.dropdown.Option('3')
-        
-        
-        
-    #     )
+    #ajeitar drop box
+    category_product =ft.Dropdown()
+
+
+
     description_product = ft.TextField(label='descrição do produto')
     # tem que ajeitar o price_product para aceitar float, tem que ver primeiro nO BD isso
     price_product =ft.TextField(label='preço do produto', input_filter=ft.NumbersOnlyInputFilter())
     quantity_product = ft.TextField(label='quantidade de protudo', input_filter=ft.NumbersOnlyInputFilter() )
     submite_button = ft.ElevatedButton('enviar', on_click=submite_button_action)
 
-    page.add( nome_product,description_product,price_product,quantity_product,submite_button)
+    page.add( category_product,nome_product,description_product,price_product,quantity_product,submite_button)
 
 ft.app(target=produtos_view)
